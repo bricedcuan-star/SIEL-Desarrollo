bc4464185a5d77eb60ecb2ca7fad5ada47cc0524
bricedcuan-star <bricedcuan@gmail.com>
2026-06-25 16:09:19 -0500

    Change route path from '/Index' to '/'

--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute('/Index')({
+export const Route = createFileRoute('/')({
   component: () => <div>¡Hola, el archivo Index funciona!</div>,
 })
